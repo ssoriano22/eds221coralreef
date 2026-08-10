0d1a742b228a046b36a3f27b98357b4d0a426940
ssoriano22 <smsoriano277@gmail.com>
2026-08-10 16:04:39 -0700

    Updating roll_dice.R script w/ comments

--- roll_dice.R
+++ roll_dice.R
@@ -3,7 +3,12 @@
 # Roll Dice R Script
 # Description: an R script to randomly generate two die rolls.
 
+#Set random seed
 set.seed(123)
+
+#Assign numerics to d6 die
 die <- 1:6
+#Roll two die (w/ replacement - 1:6 are available for both die rolls)
 rolls <- sample(die, size = 2, replace = TRUE)
+#Store sum of both d6 rolls
 roll_total <- sum(rolls)
